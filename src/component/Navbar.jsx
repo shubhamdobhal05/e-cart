36b19923c0a06d4848ee9b57dc2d05d97d880582
shubhamdobhal05 <shubhamdobhal05@gmail.com>
2022-04-25 19:31:49 +0530

    file updated

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,41 +1,41 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 shadow-sm">
+  <div class="container">
+    <NavLink className="navbar-brand fw-bold fs-4" to="#">E-Commerce APP</NavLink>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav mx   -auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+          <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+          <NavLink className="nav-link" to="/products">Products</NavLink>
         </li>
+        <li class="nav-item">
+          <NavLink className="nav-link" to="/about">About</NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink className="nav-link" to="/contact">Contact</NavLink>
+        </li>
+        
+        
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      <div className="buttons">
+            <NavLink to="/login" className="btn btn-outline-dark">
+            <i className="fa fa-sign-in me-l"></i>Login</NavLink>
+            <NavLink to="/register" className="btn btn-outline-dark ms-2">
+            <i className="fa fa-user-plus me-l "></i>Register</NavLink>
+            <NavLink to="/cart" className="btn btn-outline-dark ms-2">
+            <i className="fa fa-shopping-cart me-l "></i> (0)</NavLink>
+      </div>
+
     </div>
   </div>
 </nav>
