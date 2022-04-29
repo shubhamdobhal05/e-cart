df4419e596c0a40fafaf3fdb79c290939f17aae8
Shubham Dobhal <shubhamdobhal05@gmail.com>
2022-04-29 12:47:04 +0530

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 
+
 import './App.css';
+import Home from './component/Home';
 import Navbar from './component/Navbar';
+import Products from './component/Products';
+import { Routes, Route } from "react-router-dom";
+import ProductDetails from './component/ProductDetails';
+import Login from "./component/login";
+import Register from "./component/register";
+import Cart from './component/Cart';
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path='/' element= {<Home/>}/>
+        <Route path='/products' element={<Products/>}/>
+        <Route path="/products/:id" element={<ProductDetails/>}/>
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/cart' element={<Cart/>}/>
+        </Routes>
+      
     </>
   );
 }
 
 export default App;
+
