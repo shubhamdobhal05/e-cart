0b8baea993de03749711d45bd790f5110780e96b
shubhamdobhal05 <shubhamdobhal05@gmail.com>
2022-04-29 12:38:34 +0530

    Changes made

--- src/component/ProductDetails.jsx
+++ src/component/ProductDetails.jsx
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from "react";
-import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addItem } from "../redux/action"
+import { NavLink, useParams } from "react-router-dom";
+import Skeleton from "react-loading-skeleton";
 
 
 const ProductDetails = () => {
@@ -7,6 +10,11 @@ const ProductDetails = () => {
     const {id} = useParams();
     const [product, setProduct] = useState([]);
     const [loading, setLoading] = useState(false);
+
+    const dispatch = useDispatch();
+    const addProduct = (product) => {
+        dispatch(addItem(product));
+    }
 
     useEffect(() => {
         const getProducts = async () => {
@@ -22,7 +30,18 @@ const ProductDetails = () => {
     const Loading = () => {
         return (
             <>
-                Loading.....
+                <div className="col-md-6">
+                    <Skeleton height={400}/>
+                </div>
+                <div className="col-md-6" style={{lineHeight:2}}>
+                    <Skeleton height={50} width={300}/>
+                    <Skeleton height={75}/>
+                    <Skeleton height={25} width={150}/>
+                    <Skeleton height={50}/>
+                    <Skeleton height={150}/>
+                    <Skeleton height={50} width={100} style={{marginLeft:6}}/>
+                </div>
+                
             </>
         )
     }
@@ -34,6 +53,30 @@ const ProductDetails = () => {
                 <div className="col-md-6">
                     <img src={product.image} alt={product.title} height="400px" width="400px"/>
                 </div>
+                <div className="col-md-6">
+                    <h4 className="text-uppercase text-black-50">
+                        {product.category}
+                    </h4>
+                    <h1 className="display-5">
+                        {product.title}
+                    </h1>
+                    <p className="lead fw-bolder">
+                        Rating {product.rating && product.rating.rate}
+                        <i className="fa fa-star"></i>
+                    </p>
+                    <h3 className="display-6 fw-bold my-4">
+                        ${product.price}
+                    </h3>
+                    <p className="lead">
+                        {product.description}
+                    </p>
+                    <NavLink className="btn btn-outline-dark px-4 py-2" onClick= {() => addProduct(product)}>
+                        Add to Cart
+                    </NavLink>
+                    <NavLink to="/login" className="btn btn-dark mx-2 px-4 py-2">
+                        Place Order
+                    </NavLink>
+                </div>
             </>
         )
 
@@ -43,8 +86,8 @@ const ProductDetails = () => {
 
     return (
         <div>
-            <div className="container">
-                <div className="row">
+            <div className="container py-5">
+                <div className="row py-5">
                     {loading ? <Loading/> : <ShowProduct/>}
                 </div>
             </div>
