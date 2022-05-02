6ada07015138af78cdd0911531d619fbd688314f
Shubham Dobhal <shubhamdobhal05@gmail.com>
2022-05-02 22:28:27 +0530

    Update Cart.jsx

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import {  useDispatch } from "react-redux";
-import deleteItem from "../redux/action/index";
+import { NavLink } from "react-router-dom";
+import { delItem } from "../redux/action/index";
 
 
 
 
 const Cart = () => {
-    const state = useSelector((state) => state.addItem);
+    const state = useSelector((state) => state.handleCart);
     const dispatch = useDispatch();
+
+    const handleClose = (item) => {
+        dispatch(delItem(item))
+    }
 
     const cartItems = (cartItem) => {
 
@@ -17,14 +22,14 @@ const Cart = () => {
             
             <div className="px-4 my-5 bg-light rounded-3" key={cartItem.id}>
                 <div className="container py-4">
-                    <button className="btn-close float-end" aria-label="Close"> </button>
+                    <button onClick={() => handleClose(cartItem)} className="btn-close float-end" aria-label="Close"> </button>
                     <div className="row justify-content-center">
                         <div className="col-md-4">
-                            <img src = {cartItem.img} alt = {cartItem.title} height= "200px" width="180px"/>
+                            <img src = {cartItem.image} alt = {cartItem.title} height= "200px" width="180px"/>
                         </div>
                         <div className="col-md-4">
-                            <h3></h3>
-                            <p></p>
+                            <h3>{cartItem.title}</h3>
+                            <p className="lead fw-bold">${cartItem.price}</p>
                         </div>
                     </div>
                 </div>
@@ -33,11 +38,37 @@ const Cart = () => {
         )
     }
 
+    const emptyCart = () => {
+        return (
+            <div className="px-4 my-5 bg-light rounded py-5">
+                <div className="container py-4">
+                    <div className="row">
+                        <h3>Your Cart is Empty</h3>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const button = () => {
+        return (
+            <div className="container">
+                <div className="row">
+                    <NavLink to = "/checkout" className="btn btn-outline-primary mb-5 wb-25 mx-auto">Proceed To Checkout</NavLink> 
+
+                </div>
+            </div>
+        )
+    }
 
     return (
         <>
-        
-            {state.length  !== 0 && state.map(cartItems)}
+            
+            {state.length === 0 && emptyCart()}
+            {state.length !== 0 && state.map(cartItems)}
+            {state.length !== 0 && button()}
+            
+            
         </>
     )
 }
