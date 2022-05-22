605fea0f6fdcb1708768b8fdac3e6d8bbf305cdb
Shubham Dobhal <shubhamdobhal05@gmail.com>
2022-05-22 21:48:42 +0530

    Update Cart.jsx

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import {  useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { delItem } from "../redux/action/index";
+import { deleteItem } from "../redux/action/index";
 
 
 
