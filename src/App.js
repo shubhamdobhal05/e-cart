f9cdda334c7a7c81c12b56b5161c2574f06ac639
Shubham Dobhal <shubhamdobhal05@gmail.com>
2022-05-22 21:25:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Navbar from './component/Navbar';
 import Products from './component/Products';
 import { Routes, Route } from "react-router-dom";
 import ProductDetails from './component/ProductDetails';
-import Login from "./component/login";
-import Register from "./component/register";
 import Cart from './component/Cart';
 import Checkout from "./component/Checkout";
 
@@ -18,8 +16,6 @@ function App() {
         <Route path='/' element= {<Home/>}/>
         <Route path='/products' element={<Products/>}/>
         <Route path="/products/:id" element={<ProductDetails/>}/>
-        <Route path='/login' element={<Login/>} />
-        <Route path='/register' element={<Register/>} />
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/checkout' element={<Checkout/>}/>
         </Routes>
