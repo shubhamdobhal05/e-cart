76138d15e55f51aaf4bebba2cc70fa3d6ef8bae2
Shubham Dobhal <shubhamdobhal05@gmail.com>
2022-05-22 22:03:01 +0530

    Update Products.jsx

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -8,9 +8,10 @@ const Products = () => {
     const [data, setData] = useState([])
     const [filter, setFilter] = useState(data);
     const [loading, setLoading] = useState(false);
-    let componentMounted = true;
+    
 
     useEffect(() => {
+        let componentMounted = true;
         const getProducts = async () => {
             setLoading(true);
             const res = await fetch("https://fakestoreapi.com/products");
