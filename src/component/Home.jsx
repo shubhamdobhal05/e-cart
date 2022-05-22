74ec8050dca46c0c419452fbbb4e4c74dbc0a024
Shubham Dobhal <shubhamdobhal05@gmail.com>
2022-05-22 21:41:18 +0530

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className="hero">
             <div class="card bg-dark text-white border-0">
-            <img src="/assests/bgim.jpg" class="card-img" alt="Background" height= "550px"/>
+            <img src="/assests/bgi.webp" class="card-img" alt="Background" height= "550px"/>
             <div class="card-img-overlay d-flex flex-column justify-content-center">
                 <div className="container">
                     <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
